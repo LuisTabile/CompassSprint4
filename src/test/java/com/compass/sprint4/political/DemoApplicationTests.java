23bd2bddffdaada4ac9709909f4f26786326b5ca
LuisTabile <luistabile@github.com>
2022-11-28 21:40:52 -0300

    feat: tests for controllers

--- src/test/java/com/compass/sprint4/political/DemoApplicationTests.java
+++ src/test/java/com/compass/sprint4/political/DemoApplicationTests.java
@@ -1,13 +1,37 @@
-package com.compass.sprint4.demo;
+package com.compass.sprint4.political;
 
+import com.compass.sprint4.political.controller.AssociadosController;
+import com.compass.sprint4.political.controller.PartidosController;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private AssociadosController associadosController;
+    @Autowired
+    private PartidosController partidosController;
+    private String API = "http://localhost:8080/associados";
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+        //GET ASSOCIADOS
+    void case1() throws Exception {
+
+    }
 
 }
---
+++ src/test/java/com/compass/sprint4/political/controller/PartidosControllerTest.java
@@ -0,0 +1,42 @@
+package com.compass.sprint4.political.controller;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+
+class PartidosControllerTest {
+
+    @Test
+    void cadastroPartidos() {
+    }
+
+    @Test
+    void listar() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/partidos")).build();
+        CompletableFuture cf = client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(HttpResponse::body).thenAccept(System.out::println);
+        System.out.println("passed");
+        HttpResponse<String> result = (HttpResponse<String>) cf.join();
+    }
+
+    @Test
+    void detalhes() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/partidos/5")).build();
+        CompletableFuture cf = client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(HttpResponse::body).thenAccept(System.out::println);
+        System.out.println("passed");
+        HttpResponse<String> result = (HttpResponse<String>) cf.join();
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
